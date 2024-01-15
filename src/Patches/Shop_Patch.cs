2b5f98b5973f0442dec6166d64c1e495eb9ea176
t0stiman <18124323+t0stiman@users.noreply.github.com>
2024-01-15 16:24:35 +0100

    betterer sign placement

--- src/Patches/Shop_Patch.cs
+++ src/Patches/Shop_Patch.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using DV.Shops;
 using HarmonyLib;
-using I2.Loc;
 using TMPro;
 using UnityEngine;
 using Object = UnityEngine.Object;
@@ -14,6 +13,7 @@ namespace custom_item_mod.Patches;
 [HarmonyPatch(nameof(Shop.Awake))]
 public class Shop_Awake_Patch
 {
+	// i hope you like spaghetti (code)
 	private static void Postfix(ref Shop __instance)
 	{
 		var scanModules = __instance.scanItemResourceModules;
@@ -22,15 +22,11 @@ public class Shop_Awake_Patch
 			Main.Error($"{nameof(Shop_Awake_Patch)}: {nameof(__instance.scanItemResourceModules)} not init yet");
 			return;
 		}
-
-		var scanModuleTransforms = scanModules.Select(something => something.transform).ToArray();
 		
-		// ==========================================
+		var scanModuleTransforms = __instance.scanItemResourceModules.Select(something => something.transform).ToArray();
 
 		float minimumX = scanModuleTransforms.Min(something => something.localPosition.x);
 		float maximumY = scanModuleTransforms.Max(something => something.localPosition.y);
-		
-		// ==========================================
 		
 		var ordenedModuleTransforms = new List<List<Transform>>();
 
@@ -55,37 +51,45 @@ public class Shop_Awake_Patch
 			}
 		}
 
-		float verticalOffset;
-		float horizontalOffset;
-		if (ordenedModuleTransforms.Count < 2)
+		var offset = new Vector2();
+		try
 		{
-			Main.Warning($"Shop {__instance.gameObject.name} has less then 2 rows? Assuming offsets");
-			verticalOffset = 0.288f;
-			horizontalOffset = 0.6f;
+			offset.y = Math.Abs(ordenedModuleTransforms[0][0].localPosition.y -
+			                    ordenedModuleTransforms[1][0].localPosition.y);
+			offset.x = Math.Abs(ordenedModuleTransforms[0][0].localPosition.x -
+			                    ordenedModuleTransforms[0][1].localPosition.x);
 		}
-		else
+		catch (IndexOutOfRangeException e)
 		{
-			verticalOffset = Math.Abs(ordenedModuleTransforms[0][0].localPosition.y -
-			                              ordenedModuleTransforms[1][0].localPosition.y);
-			horizontalOffset = Math.Abs(ordenedModuleTransforms[0][0].localPosition.x -
-			                                ordenedModuleTransforms[0][1].localPosition.x);
-		}
-
-		// ==========================================
-		
-		{
-			int itemsPerRow = ordenedModuleTransforms.Max(x => x.Count);
-			var startPos = new Vector2(minimumX, maximumY);
+			e.ToString(); //get rid of the 'unused variable' warning
 			
-			int row = 1;
+			Main.Warning($"Shop {__instance.gameObject.name} has less then 2 rows/cols? Assuming offsets");
+			offset.y = 0.288f;
+			offset.x = 0.6f;
+		}
+
+		foreach (var item in ItemModsFinder.CustomItems)
+		{
+			AddItemToShop(item, ref __instance);
+		}
+
+		{
+			var extraItemsPerRow = 2;  //fit more items in a row
+			int itemsPerRow = ordenedModuleTransforms.Max(x => x.Count) + extraItemsPerRow;
+			var startPos = new Vector3(minimumX - extraItemsPerRow*offset.x/2.0f, maximumY, scanModuleTransforms[0].localPosition.z);
+
+			//not sure if its necessary to do this again to include the custom items, but lets be safe
+			scanModuleTransforms = __instance.scanItemResourceModules.Select(something => something.transform).ToArray();
+			
+			int row = 0;
 			int column = 0;
-
-			for (var i = 0; i < ItemModsFinder.CustomItems.Count; i++)
+			
+			foreach (var t in scanModuleTransforms)
 			{
-				var localPosition = new Vector3(startPos.x + column * horizontalOffset, startPos.y + row * verticalOffset,
-					scanModuleTransforms[0].localPosition.z);
-				AddItemToShop(ItemModsFinder.CustomItems[i], ref __instance, scanModules[0].gameObject, localPosition);
-
+				
+				t.localPosition = new Vector3(startPos.x + column * offset.x, startPos.y - row * offset.y,
+					startPos.z);
+				
 				column++;
 				if (column >= itemsPerRow)
 				{
@@ -93,9 +97,17 @@ public class Shop_Awake_Patch
 					row++;
 				}
 			}
+
+			// recenter the signs
+			var oldRowCount = ordenedModuleTransforms.Count;
+			var rowCountDelta = row+1 - oldRowCount;
+			var yMovePls = rowCountDelta * offset.y / 2.0f;
+			
+			foreach (var idk in __instance.scanItemResourceModules)
+			{
+				idk.gameObject.transform.localPosition += new Vector3(0, yMovePls, 0);
+			}
 		}
-		
-		
 	}
 
 	private static bool NearlyEqual(float a, float b, float margin)
@@ -103,19 +115,16 @@ public class Shop_Awake_Patch
 		return a==b || ( a < b + margin && a > b - margin);
 	}
 
-	private static void AddItemToShop(CustomItem anItem, ref Shop aShop, GameObject existingScanModuleObject, Vector3 localPosition)
+	private static void AddItemToShop(CustomItem anItem, ref Shop aShop)
 	{
+		var existingScanModuleObject = aShop.scanItemResourceModules[0].gameObject;
 		var newScanModuleObject =
 			Object.Instantiate(existingScanModuleObject, existingScanModuleObject.transform.parent);
-
-		//todo better sign positioning system
-		newScanModuleObject.transform.localPosition = localPosition;
 
 		newScanModuleObject.name = $"{existingScanModuleObject.name}({anItem.ItemSpec.LocalizedName})";
 
 		var module = newScanModuleObject.GetComponent<ScanItemCashRegisterModule>();
 		module.sellingItemSpec = anItem.ItemSpec;
-		// Main.Log($"item spec set on {newScanModuleObject.name}");
 
 		//name
 		{
